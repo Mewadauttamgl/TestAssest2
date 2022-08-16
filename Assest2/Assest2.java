1faf62c599ef134379d9bc672526f89ef7fea0ea
Mewadauttamgl <110605686+Mewadauttamgl@users.noreply.github.com>
2022-08-16 10:55:17 +0530

    Add files via upload

---
+++ Assest2/Assest2.java
@@ -0,0 +1,57 @@
+package com.java.jpa.find;
+import javax.persistence.*;
+
+import com.java.jpa.employee.Employee;
+import java.util.List;
+public class Assest2 {
+
+	public static void main(String[] args) {
+		
+		 EntityManagerFactory emf=Persistence.createEntityManagerFactory("Employee_details");
+
+		    EntityManager em=emf.createEntityManager();
+		    em.getTransaction().begin();
+
+		    Employee e1=new Employee();
+
+		   e1.setEmpno(101);
+		   e1.setName("Uttam");
+		   e1.setDesignation("SDE");
+		   e1.setSalary(100000);
+		   Employee e2=new Employee();
+		   
+
+		   e2.setEmpno(102);
+		   e2.setName("Shiavm");
+		   e2.setDesignation("SDE");
+		   e2.setSalary(100000);
+ Employee e3=new Employee();
+          e3.setEmpno(102);
+         e3.setName("Shiavm");
+         e3.setDesignation("SDE");
+         e3.setSalary(100000);
+
+		  		   em.persist(e1);
+
+		    em.persist(e2);
+
+		    em.persist(e3);
+
+		    em.getTransaction().commit();
+		    
+		   
+		    Query q= em.createQuery("from Employee");
+
+		    List <Employee> list= q.getResultList();
+
+		    for(Employee se:list)
+
+		    {
+
+		     System.out.println(se.getName()+" "+se.getEmpno()+" "+" "+se.getDesignation()+" " +se.getSalary());
+
+		    }
+
+	}
+
+}
