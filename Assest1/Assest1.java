3bd23b3c5ca047816b17e5639007fc4e3cbaa158
Mewadauttamgl <110605686+Mewadauttamgl@users.noreply.github.com>
2022-08-16 10:09:42 +0530

    Add files via upload

---
+++ Assest1/Assest1.java
@@ -0,0 +1,65 @@
+package com.java;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class Assest1 {
+  @RequestMapping("/hello")
+
+  public String display(@RequestParam("name") String name,@RequestParam("pass") String pass,@RequestParam("email") String email,@RequestParam("addr") String addr,
+		  @RequestParam("Desig") String Desig,@RequestParam("sal") String Sal, Model m) throws  Exception
+
+  {
+
+    if(pass.equals("admin"))
+
+    {
+
+      String msg="Hello "+ name+" registration is successfull Your Inserted data is---->";
+      String name1="E_Name :-"+name;
+      String email1="E_Email :- "+email;
+      String address="E_Address :- "+addr;
+      String designation="E_Designation :- "+Desig;
+      String Salary="E_Salary :- "+Sal;
+
+      
+
+      m.addAttribute("message", msg);
+      m.addAttribute("message1",name1 );
+      m.addAttribute("message3",email1);
+      m.addAttribute("message4",address);
+      m.addAttribute("message5",designation);
+      m.addAttribute("message6",Salary);
+      
+            return "viewpage";
+
+    }
+
+    else
+
+    {
+
+      String msg="Sorry "+ name+". You are not existing employee plz enter correct password provided by company";
+
+      m.addAttribute("message", msg);
+
+      return "errorpage";
+
+    }
+
+  }
+
+}
+
---
+++ Assest1/index.jsp
@@ -0,0 +1,27 @@
+<html>
+
+<body>
+
+
+<form action="hello" style="background-color: #DBF9FC">
+
+E_Name : <input type="text" name="name"/> <br><br>
+
+Password : <input type="text" name="pass"/> <br><br>
+
+E_Email : <input type="text"    name="email"/> <br><br>
+
+E_Address  : <input type="text" name="addr"/> <br><br>
+
+E_Designation  : <input type="text" name="Desig"/> <br><br>
+
+Salary : <input type="text" name="sal"/> <br><br>
+
+<input type="submit" name="submit">
+
+</form>
+
+</body>
+
+</html>
+
